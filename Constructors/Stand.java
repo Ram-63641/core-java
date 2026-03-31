2f5b930dd072706200c9eb19228763fd98431e16
Bhargav Mahendrakar <b3909526@gmail.com>
2026-03-31 23:50:58 +0530

    Constructors and instances

---
+++ Constructors/Stand.java
@@ -0,0 +1,50 @@
+class Stand {
+	 
+	 long id;
+	 String brand;
+	 String type;
+	 int height;
+	 String color;
+	 boolean adjustable;
+	 int price;
+	 String material;
+	 String origin;
+	 double weight;
+	 double width;
+	 double length;
+	 boolean foldable;
+	 int capacity;
+	 String shape;
+	 String design;
+	 boolean portable;
+	 String usage;
+	 String availability;
+	 boolean durable;
+	 String baseType;
+	 
+	 
+	 Stand(){
+		 
+		 id = 55667788L;
+		 brand = "Generic";
+		 type = "Mobile Stand";
+		 height = 30;
+		 color = "Black";
+		 adjustable = true;
+		 price = 500;
+		 material = "Plastic";
+		 origin = "China";
+		 weight = 0.8;
+		 width = 10.5;
+		 length = 15.0;
+		 foldable = true;
+		 capacity = 5;
+		 shape = "Rectangle";
+		 design = "Simple";
+		 portable = true;
+		 usage = "Mobile Holder";
+		 availability = "Available";
+		 durable = true;
+		 baseType = "Flat";
+	 }
+}
