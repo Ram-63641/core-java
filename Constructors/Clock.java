33dd6691cfc151d8a48a59018899769bad41e19e
Bhargav Mahendrakar <b3909526@gmail.com>
2026-03-31 23:42:29 +0530

    constuctor and instances

---
+++ Constructors/Clock.java
@@ -0,0 +1,62 @@
+class Clock{
+	
+String brand;
+String color;
+double price;
+boolean digital;
+int warranty;
+String type;
+String shape;
+double weight;
+boolean smart;
+String displayType;
+int batteryLife;
+double size;
+String material;
+boolean alarm;
+String timeFormat;
+int brightnessLevel;
+double accuracy;
+String origin;
+boolean waterproof;
+String usage;
+String powerSource;
+int buttons;
+double thickness;
+String mountingType;
+boolean silentMode;
+String design;
+	
+	public Clock(){
+			
+		brand = "Titan";
+		color = "white";
+		price = 4000;
+		digital = false;
+		warranty = 2;
+		type ="Office";
+		shape = "circle";
+		weight = 1;
+		smart = false;
+		displayType = "Analog";
+		batteryLife = 100;
+		size = 10;
+		material = "plastic";
+		alarm = true;
+		timeFormat = "India";
+		brightnessLevel = 6;
+		accuracy = 4.4;
+		origin = "India";
+		waterproof = false;
+		usage = "office";
+		powerSource = "Battery";
+		buttons = 4;
+		thickness = 9;
+		mountingType = "stand";
+		silentMode = true;
+		design = "stone";
+		
+	}
+	
+	
+}
---
+++ Constructors/Monitor.java
@@ -0,0 +1,50 @@
+class Monitor {
+	 
+	 long id;
+	 String brand;
+	 String type;
+	 int refreshRate;
+	 String resolution;
+	 boolean curved;
+	 int screenSize;
+	 String panelType;
+	 String origin;
+	 double weight;
+	 double brightness;
+	 double responseTime;
+	 boolean touch;
+	 int ports;
+	 String aspectRatio;
+	 String material;
+	 boolean smart;
+	 String connectivity;
+	 String availability;
+	 boolean wallMount;
+	 String usage;
+	 
+	 
+	 Monitor(){
+		 
+		 id = 11223344L;
+		 brand = "Samsung";
+		 type = "LED";
+		 refreshRate = 144;
+		 resolution = "1920x1080";
+		 curved = false;
+		 screenSize = 27;
+		 panelType = "IPS";
+		 origin = "South Korea";
+		 weight = 4.5;
+		 brightness = 300.5;
+		 responseTime = 1.5;
+		 touch = false;
+		 ports = 3;
+		 aspectRatio = "16:9";
+		 material = "Plastic";
+		 smart = true;
+		 connectivity = "HDMI";
+		 availability = "Available";
+		 wallMount = true;
+		 usage = "Gaming";
+	 }
+}
