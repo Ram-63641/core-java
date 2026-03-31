980fb86b4d9adc38d8641aba212fa8f4251d5692
Bhargav Mahendrakar <b3909526@gmail.com>
2026-03-31 23:52:24 +0530

    Constructors and instances

---
+++ Constructors/Bangle.java
@@ -0,0 +1,59 @@
+class Bangle{
+	
+	String brand;
+	String color;
+	double price;
+	boolean adjustable;
+	int quantity;
+	String material;
+	String type;
+	double diameter;
+	boolean handmade;
+	String design;
+	int weight;
+	double thickness;
+	String finish;
+	boolean stoneStudded;
+	String usage;
+	String origin;
+	boolean antique;
+	int size;
+	String pattern;
+	int durability;
+	double shinelevel;
+	String gender;
+	boolean ecofriendly;
+	String occasion;
+	String plating;
+	String collection;
+	
+	Bangle(){
+		
+		brand = "joyalukas";
+		color = "yellow";
+		price = 3000;
+		adjustable = false;
+		quantity = 40;
+		material = "gold";
+		type = "female";
+		diameter = 5;
+		handmade = true;
+		design = "good";
+		weight = 200;
+		thickness = 7;
+		finish = "smooth";
+		stoneStudded = true;
+		usage = "function";
+		origin = "India";
+		antique = true;
+		size = 5;
+		pattern = "new";
+		durability = 7;
+		shinelevel = 5;
+		gender="female";
+		ecofriendly = false;
+		occasion = "function";
+		plating = "high";
+		collection = "market";
+	}
+}
